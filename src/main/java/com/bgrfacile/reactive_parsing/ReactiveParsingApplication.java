95059cdbd94ee70bba3d5cec4dd072e4cadbfa00
benaja-bendo <benaja.bendo02@gmail.com>
2024-12-04 09:36:00 +0100

    premier commit

---
+++ src/main/java/com/bgrfacile/reactive_parsing/ReactiveParsingApplication.java
@@ -0,0 +1,13 @@
+package com.bgrfacile.reactive_parsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveParsingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveParsingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bgrfacile/reactive_parsing/ReactiveParsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgrfacile.reactive_parsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveParsingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
